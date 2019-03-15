b4810c3a12f1791b10e9fada1fc2e66555f1399e
zh-dmit <zh-dmit@yandex.ru>
2019-03-15 23:18:21 +0300

    1.3. JavaDoc. Code convention[#120005]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,19 @@
 package ru.job4j.calculate;
 
+/**
+ * print string Hello world!
+ *
+ * @author Evgeny Dmitriev 
+ */
+ 
+
 public class Calculate {
+	
+	/**
+	*Method main
+	*@return print string Hello world!
+	*/
+	
 	
 	public static void main(String [] args){
 		System.out.println("Hello world!");
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Dmitriev Evgeny
+* @version 1.1
+* @since 15.03.19
+*/
+package ru.job4j.calculate;
