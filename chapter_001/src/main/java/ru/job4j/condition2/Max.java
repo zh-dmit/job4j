a9c3361927b1bb807e2c0980d7a95344d36c9f4c
zh-dmit <zh-dmit@yandex.ru>
2019-04-02 20:44:10 +0300

    4.2. Максимум из двух чисел[#119965]

--- chapter_001/src/main/java/ru/job4j/condition2/Max.java
+++ chapter_001/src/main/java/ru/job4j/condition2/Max.java
@@ -4,16 +4,4 @@ public class Max {
     public int max(int first, int second) {
         return first > second ? first : second;
     }
-
-    public int firstMax(int first, int second) {
-        return first > second ? 1 : 0;
-    }
-
-    public int secondMax(int first, int second) {
-        return first < second ? 1 : 0;
-    }
-
-    public int equalsFS(int first, int second) {
-        return first == second ? 1 : 0;
-    }
 }
--- chapter_001/src/test/java/ru/job4j/condition2/MaxTest.java
+++ chapter_001/src/test/java/ru/job4j/condition2/MaxTest.java
@@ -16,21 +16,21 @@ public class MaxTest {
     @Test
     public void whenFirstMax() {
         Max max = new Max();
-        int result = max.firstMax(4, 2);
-        assertThat(result, is(1));
+        int result = max.max(2, 1);
+        assertThat(result, is(2));
     }
 
     @Test
     public void secondMax() {
         Max max = new Max();
-        int result = max.secondMax(1, 6);
-        assertThat(result, is(1));
+        int result = max.max(1, 2);
+        assertThat(result, is(2));
     }
 
     @Test
     public void equalsFS() {
         Max max = new Max();
-        int result = max.equalsFS(1, 6);
-        assertThat(result, is(0));
+        int result = max.max(1, 1);
+        assertThat(result, is(1));
     }
 }
