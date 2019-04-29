7838f007dbdaab2e754f8d4208cb77d81ec9153b
zh-dmit <zh-dmit@yandex.ru>
2019-04-29 21:33:53 +0300

    6.7. Квадратный массив заполнен true или false по диагоналям.[#119985]

---
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -0,0 +1,25 @@
+package ru.job4j.array;
+
+/**
+ * class MatrixCheck.
+ * Проверяет совпадают ли значения по диоганалям матрицы.
+ *
+ * @author Evgeny.
+ * @versiov 1.1
+ * @since 29.04.19
+ */
+public class MatrixCheck {
+    /**
+     * @param data двумерный массив (матрица).
+     * @return true , false (если совпадают и если не совпадают).
+     */
+    public boolean mono(boolean[][] data) {
+        boolean result = true;
+        for (int indexA = 0; indexA < data.length - 1; indexA++) {
+            if ((data[indexA][indexA] != data[indexA + 1][indexA + 1]) && (data[indexA][(data.length - 1) - indexA] != data[indexA + 1][(data.length - 2) - indexA])) {
+                result = false;
+            }
+        }
+        return result;
+    }
+}
--- chapter_001/src/main/java/ru/job4j/array/package-info.java
+++ chapter_001/src/main/java/ru/job4j/array/package-info.java
@@ -2,7 +2,7 @@
 * Package for array task.
 * 
 * @author Evgeny.
-* @version 1.7
-* @since 25.04.19
+* @version 1.8
+* @since 29.04.19
 */
 package ru.job4j.array;
